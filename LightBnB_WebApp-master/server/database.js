f41b099560126d06f9dca4a89979036a5a45ca29
julimancan <julian.m.bustos@gmail.com>
2020-06-25 15:26:21 -0400

    changed the getallreservations function for a query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -78,7 +78,15 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT properties.* FROM reservations
+  JOIN users ON reservations.guest_id = users.id
+  JOIN properties ON reservations.property_id = properties.id
+  WHERE reservations.guest_id = $1
+  LIMIT $2  
+  `, [guest_id, limit])
+  .then(res => res.rows)
+  // return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
